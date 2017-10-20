de98e3cc22aa7d31c4b5e0c796e39fb2c2df91ae
Michael K <kux.michael2@googlemail.com>
2017-10-20 21:31:51 +0200

    Add files via upload

---
+++ RCON/config.php
@@ -0,0 +1,10 @@
+<?php
+$rconHost = "";
+$rconPort = 25575;
+$rconPassword = "";
+
+$host = "";
+$port = 25575;
+$password = "";
+$timeout = 3; 
+?>
---
+++ data/MySqlconfig.php
@@ -0,0 +1,11 @@
+<?php
+// Mysql
+$mysql = "mysql:host=localhost;dbname=MinecraftApp";
+$dbuser = "root";
+$pass = "";
+//RcoN
+$host = "";												
+$port = 25575;														
+$password = "";										
+$timeout = 3;
+?>
